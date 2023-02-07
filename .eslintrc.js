bfbadbdcb875508dc017e27049df42bb8b04768a
CarlosOliveira-23 <carlos.j.oliver18@gmail.com>
2023-02-06 23:19:19 -0300

    Error Correction

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,30 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es6: true,
   },
   extends: [
     'plugin:react/recommended',
     'airbnb',
   ],
-  overrides: [
-  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
   parser: 'babel-eslint',
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
     sourceType: 'module',
   },
   plugins: [
     'react',
   ],
   rules: {
-    "react/jsx-filename-extension": 0
-    "react/state-in-constructor": 0
+    "react/jsx-filename-extension": 0,
+    "react/state-in-constructor": 0,
     "react/forbid-prop-types": 0
   },
 };
